052078ed795b21c6650322d942ad46fc7af886ff
Ujjwal Kishor <decentgamer999@gmail.com>
2026-04-14 03:43:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -363,3 +363,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log("Service Worker Registered"));
+}
